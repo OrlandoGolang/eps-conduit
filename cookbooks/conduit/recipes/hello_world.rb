80bb019a12458a63b86567c16412fa0b723223e7
Scott Crespo <screspo@highcornergroup.com>
2015-11-27 12:03:34 -0500

    hello_world cookbook clones and builds eps-conduit-hello application, which will be used for testing eps-conduit functionality

---
+++ cookbooks/conduit/recipes/hello_world.rb
@@ -0,0 +1,45 @@
+# Clones hello world application and builds from source
+
+ogo_path = '/vagrant/src/github.com/OrlandoGolang'
+hello_path = File.join(ogo_path, 'eps-conduit-hello')
+
+package 'Install Git' do
+  package_name 'git'
+end
+
+directory ogo_path do
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0755'
+  action:create
+end
+
+cookbook_file '/home/vagrant/.ssh/eps-conduit-hello' do
+  source 'eps-conduit-hello'
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0600'
+  action :create
+end
+
+cookbook_file '/home/vagrant/.ssh/git_wrapper.sh' do
+  source 'git_wrapper.sh'
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0755'
+  action :create
+end
+
+git "Clone eps-conduit-hello" do
+  repository 'git@github.com:OrlandoGolang/eps-conduit-hello.git'
+  action :sync
+  user 'vagrant'
+  group 'vagrant'
+  ssh_wrapper '/home/vagrant/.ssh/git_wrapper.sh'
+  destination hello_path
+end
+
+execute 'Build eps-conduit-hello' do
+  cwd hello_path
+  command 'go get ./... && go install'
+end
