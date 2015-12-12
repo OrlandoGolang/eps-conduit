e6e9304aacd19c4807d80330f1069cb87666ddfd
Scott Crespo <screspo@highcornergroup.com>
2015-12-12 11:57:51 -0500

    included supervisor recipe for provisioning. successfully ran two hello world processes behing load balancer. however, port forwarding from browser is still not working properly.

--- cookbooks/conduit/metadata.rb
+++ cookbooks/conduit/metadata.rb
@@ -7,3 +7,4 @@ recipe "conduit::supervisor", "Installs and configures supervisor for hello worl
 recipe "conduit::hello_world", "Simple hello world application to test eps-conduit"
 recipe "conduit::compile_daemon", "Installs Compile Daemon for easy development"
 recipe "conduit::build", "Runs initial build of project"
+recipe "conduit::supervisor", "Install and runs supervisor"
